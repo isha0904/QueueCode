b0768a09b883cfa30fde80b114401406ffbe89a2
Isha Baviskar <128312634+isha0904@users.noreply.github.com>
2024-04-05 09:10:37 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package DSACraft;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*****WELCOME TO MUSIC CONNECT*****");
+	}
+
+}
